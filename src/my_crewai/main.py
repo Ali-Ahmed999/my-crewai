815eedf73807150c5c692fe74073783700ffcffe
Ali-Ahmed999 <hafizaliahmed2004@gmail.com>
2025-03-26 23:38:12 +0500

    adding nodes

--- src/my_crewai/main.py
+++ src/my_crewai/main.py
@@ -4,15 +4,14 @@ from crewai.flow.flow import Flow, listen, start # type: ignore
 class ExampleFlow(Flow):
     @start()
     def node1(self):
-        print("im node one")
+        self.state["name"] = "Ali Ahmed"
         
-
-    def node2(self):
-        print("im node two")
         
     @listen("node1")
-    def node3(self):
-        print("im node three")
+    def node2(self):
+        print(f"hello my dear {self.state['name']}")
+        
+   
   
 def kickoff():
     print("start app...")
