8b4d2df1ff4ee6e64ca9d61deca79483f9c5b194
Ali-Ahmed999 <hafizaliahmed2004@gmail.com>
2025-03-27 00:50:17 +0500

    adding state id

--- src/my_crewai/main.py
+++ src/my_crewai/main.py
@@ -3,24 +3,29 @@ from pydantic import BaseModel
 
 class ExampleState(BaseModel):
     name: str = "Ali"
-    age: int = 20
-    edu: str = ""
+    # age: int = 20
+    # edu: str = ""
 
 
 class ExampleFlow(Flow[ExampleState]):
+    
     @start()
     def node1(self):
-        self.state.name = "Ahmed" 
-        self.state.age = 23       
+        # self.state.name = "Ahmed" 
+        # self.state.age = 23
+        return "wasil"  
         
     @listen("node1")
-    def node2(self):
-        self.state.edu = "bachelor"
-        print(f"hello my dear {self.state.name} my age is {self.state.age} my education is {self.state.edu}")
+    def node2(self,updateName):
+        self.state.name = updateName
+        # self.state.edu = "bachelor"
+        return f"hello my dear {self.state.name}"
         
    
   
 def kickoff():
     print("start app...")
     flow = ExampleFlow()
-    flow.kickoff()
+    result = flow.kickoff()
+    print(result)
+    print(flow.state)
