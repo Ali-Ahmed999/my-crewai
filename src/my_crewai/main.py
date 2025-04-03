7f2e9767ca9ec02b0f9d51929336608e46424512
Ali-Ahmed999 <hafizaliahmed2004@gmail.com>
2025-04-03 19:26:53 +0500

    flow control with routers

--- src/my_crewai/main.py
+++ src/my_crewai/main.py
@@ -30,33 +30,62 @@
 #     print(flow.state)
 
 
-from crewai.flow.flow import Flow, listen, start # type: ignore
-from litellm import completion
+# from litellm import completion
 
 
+# class ExampleFlow(Flow):
+    
+#     @start()
+#     def node1(self):
+#         user_input = input("write propmt: ")
+#         response = completion(
+#             model="gemini/gemini-1.5-flash",
+#             messages=[{"role": "user", "content": user_input}],
+#         )
+#         return response["choices"][0]["message"]["content"]
+    
+#     @listen("node1")
+#     def node2(self,name):
+#         return f"hello my dear {name}"
+        
+        
+
+
+# def kickoff():
+#     print("start app...")
+#     flow = ExampleFlow()
+#     result = flow.kickoff()
+#     print("flow output: ",result)
+    
+    
+from crewai.flow.flow import Flow, listen, start, router # type: ignore
+import random  
+  
 class ExampleFlow(Flow):
     
     @start()
-    def node1(self):
-        user_input = input("write propmt: ")
-        response = completion(
-            model="gemini/gemini-1.5-flash",
-            messages=[{"role": "user", "content": user_input}],
-        )
-        return response["choices"][0]["message"]["content"]
-    
-    @listen("node1")
-    def node2(self,name):
-        return f"hello my dear {name}"
+    def start_node(self):
+        condition_flag = random.choice(["a","b"])
+        self.state["flag"] = "condition flag: ",condition_flag
         
-        
+    @router(start_node)  
+    def condition_node(self):
+        if self.state["flag"] == "a":
+            return "a"
+        else:
+         return "b"
+     
+    @listen("a")
+    def node_a (self):
+         print("this is node a")
+         
+      
+    @listen("b")
+    def node_b(self):
+        print("this is node b")
 
 
 def kickoff():
     print("start app...")
     flow = ExampleFlow()
-    result = flow.kickoff()
-    print("flow output: ",result)
-    
-    
-        
+    flow.kickoff()
